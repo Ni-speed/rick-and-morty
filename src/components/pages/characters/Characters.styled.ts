e891691ded51480af0ed9ea4fef8aa120e07ddbd
Ni-speed <railmir@gmail.com>
2024-02-28 12:43:36 +0500

    feat: add Characters component and styles for it

---
+++ src/components/pages/characters/Characters.styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const UL = styled.ul`
+  display: grid;
+  gap: 20px;
+  grid-template-columns: 1fr 1fr 1fr;
+  place-items: center;
+  flex-wrap: wrap;
+  max-width: 1920px;
+`
---
+++ src/components/pages/characters/Characters.tsx
@@ -0,0 +1,26 @@
+import { UL } from '@/components/pages/characters/Characters.styled'
+import { CharacterCard, Typography } from '@/components/ui'
+import { useGetCharactersQuery } from '@/services/characters'
+
+export const Characters = () => {
+  const { data: characters } = useGetCharactersQuery()
+
+  console.log(characters)
+
+  // const episodes = characters.results.map()
+  return (
+    <section>
+      <Typography tag={'h1'} variant={'banner'}>
+        The Rick and Morty
+      </Typography>
+
+      <UL>
+        {characters?.results.map(character => (
+          <li key={character.id}>
+            <CharacterCard character={character} />
+          </li>
+        ))}
+      </UL>
+    </section>
+  )
+}

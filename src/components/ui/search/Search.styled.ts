4fc8b7fcaf3a4613d6c953c832d0d0b6003fe79c
Ni-speed <railmir@gmail.com>
2024-02-29 10:26:59 +0500

    style: fix Search style

--- src/components/ui/search/Search.styled.ts
+++ src/components/ui/search/Search.styled.ts
@@ -3,26 +3,34 @@ import { styled } from 'styled-components'
 export const Container = styled.div`
   position: relative;
   display: flex;
-  justify-content: space-around;
+  justify-content: flex-start;
+  gap: 20px;
+  width: 40%;
+  padding: 6 12;
 `
 export const Input = styled.input`
   all: unset;
   width: 40%;
-  border-radius: 8px;
+  border-radius: 10px;
   border: 2px solid ${Theme.colors.warning.warning500};
   padding: 10px 15px;
   color: ${Theme.colors.warning.warning500};
-  //font-weight: ${Theme.typography.fontWeights.Bold};
+  font-weight: ${Theme.typography.fontWeights.Bold};
   &:focus {
     outline: none;
   }
 `
 export const Button = styled.button`
-  background-color: #007bff;
-  color: #fff;
-  border: none;
-  border-radius: 5px;
+  background: transparent;
+  color: ${Theme.colors.warning.warning500};
+  border: 2px solid ${Theme.colors.warning.warning500};
+  border-radius: 10px;
   padding: 10px;
   cursor: pointer;
   transition: background-color 0.3s ease-in-out;
+  &:hover {
+    background: ${Theme.colors.warning.warning500};
+    color: ${Theme.colors.dark.dark900};
+    box-shadow: 0px -4px 20px rgba(274, 144, 0, 0.5);
+  }
 `

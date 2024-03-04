50c5f6ce8eeae8e9e7139168b9895b98f7934dbc
Ni-speed <railmir@gmail.com>
2024-03-04 10:15:05 +0500

    fix: add localStorage in Episodes component

--- src/components/pages/episodes/Episodes.tsx
+++ src/components/pages/episodes/Episodes.tsx
@@ -1,21 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import { EpisodesTable } from '@/components/pages/episodes/episodsTable/EpisodesTable'
 import { Pagination } from '@/components/ui'
 import { Search } from '@/components/ui/search'
+import { Button } from '@/components/ui/search/Search.styled'
 import { STypography } from '@/components/ui/typography'
 import { useGetEpisodesQuery } from '@/services/episodes'
 import { Container, Pages } from '@/styles'
 
 export const Episodes = () => {
   const [searchTerm, setSearchTerm] = useState(localStorage.getItem('episodeName') || '')
-  const [currentPage, setCurrentPage] = useState<number>(1)
+  const [currentPage, setCurrentPage] = useState(() => {
+    const storedPage = parseInt(localStorage.getItem('currentPage') || '', 10)
 
+    return isNaN(storedPage) ? 1 : storedPage
+  })
+
+  useEffect(() => {
+    localStorage.setItem('currentPage', String(currentPage))
+  }, [currentPage])
   const { data: episodes, isLoading } = useGetEpisodesQuery({ name: searchTerm, page: currentPage })
 
   const handleSearch = (term: string) => {
     setSearchTerm(term)
     localStorage.setItem('episodeName', term)
+    setCurrentPage(1)
+  }
+  const handleResetFilters = () => {
+    setSearchTerm('')
     setCurrentPage(1)
   }
 
@@ -29,7 +41,7 @@ export const Episodes = () => {
         <Pages.NavBar>
           <STypography.H1>Episodes</STypography.H1>
           <Search initialValue={searchTerm} onSearch={handleSearch} />
-          <Pages.Filter></Pages.Filter>
+          <Button onClick={handleResetFilters}>Reset Filters</Button>
           <div style={{ margin: '0 auto' }}>
             <Pagination count={episodes.info.pages} onChange={setCurrentPage} page={currentPage} />
           </div>
